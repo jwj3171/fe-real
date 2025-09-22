196bbee62d570b608bb1f97b60de0d811a2860e4
ehj <ehj9118@gmail.com>
2025-09-22 09:07:09 +0900

    랜딩 페이지 UI 구현 #1

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body>
-        {/* <Header /> */}
+        <Header /> 
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,47 @@
-import SearchInput from "@/components/common/input/SearchInput";
-import TextInput from "@/components/common/input/TextInput";
-import Image from "next/image";
+
+import styles from '@/app/landing.module.css';
+
 
 export default function Home() {
-  return <></>;
+  
+  return (
+  <div className={styles.body}>
+
+  <div className={styles.landingBanner}>
+    <img src="/assets/landing-banner.svg" alt="배경 이미지" className={styles.landingBannerImg} />
+  <div className={styles.landingBannerTextBox} >
+    <div className={styles.landingBannerTextMain}>이사 업체 어떻게 고르세요?</div>
+    <div className={styles.landingBannerTextSub}>무빙은 여러 견적을 한눈에 비교해 <br/> 
+    이사업체 선정 과정을 간편하게 바꿔드려요</div> 
+    </div>
+  </div>
+  
+  <div className={styles.landingCard}>
+      <img src="/assets/landing-card.svg" alt="배경카드" className={styles.landingCardImg} ></img>
+      <div className={styles.landingCardText} >번거로운 선정과정,<br/>이사 유형부터 선택해요</div>
+  </div>
+
+ 
+    <div className={styles.landingHero}>
+      <img src="/assets/landing-hero.svg" alt="배경히로" className={styles.landingHeroImg} ></img>
+    <div className={styles.landingHeroText} >원하는 이사 서비스를 요청하고<br/>견적을 받아보세요</div>
+  </div>
+
+  <div className={styles.landingPreview}>
+      <img src="/assets/landing-preview.svg" alt="배경프리뷰" className={styles.landingPreviewImg} ></img>
+    <div className={styles.landingPreviewText} >여러 업체의 견적을<br/>한눈에 비교하고 선택해요</div>
+  </div>
+
+
+  <div className={styles.landingFooter}>
+    <div className={styles.footercontainer}>
+      <div className={styles.appIcon}>
+        <img src="/favicon.ico" alt="앱 아이콘" ></img>
+        </div>
+      <div className={styles.footerText}>복잡한 이사 준비, 무빙 하나면 끝!</div>
+    </div>
+  </div>
+  </div>
+  ); 
+
 }
