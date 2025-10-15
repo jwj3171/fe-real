f6c128f3b5236e2253debec8bb97833ec88b7d42
ehj <ehj9118@gmail.com>
2025-10-15 16:17:22 +0900

    밸리데이션 로직 추가 #3

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -116,94 +116,114 @@ export default function SignUpPage() {
             </div>
           </div>
 
-          {/* 폼 영역 */}
-          <div className="flex w-full flex-col gap-[24px]">
-            <form
-              className="flex flex-col gap-[56px]"
-              onSubmit={handleSubmit}
-              noValidate
+          <div className="h-[1488px] bg-[#ffffff] p-[45px] md:bg-[#F9502E]">
+            <button
+              type="submit"
+              disabled={!canSubmit}
+              className={`className="w-full " cursor-pointer rounded-[16px] p-[14px] font-semibold ${
+                canSubmit
+                  ? "cursor-pointer bg-[#F9502E] text-[#FFFFFF]"
+                  : "cursor-not-allowed bg-[#D9D9D9] text-[#FFFFFF]"
+              }`}
             >
-              <div className="mx-auto flex w-full flex-col gap-[32px]">
-                {[
-                  {
-                    id: "username",
-                    label: "이름",
-                    placeholder: "성함을 입력해 주세요",
-                    type: "text",
-                  },
-                  {
-                    id: "email",
-                    label: "이메일",
-                    placeholder: "이메일을 입력해 주세요",
-                    type: "email",
-                  },
-                  {
-                    id: "phone",
-                    label: "전화번호",
-                    placeholder: "전화번호를 입력해 주세요",
-                    type: "tel",
-                  },
-                  {
-                    id: "password",
-                    label: "비밀번호",
-                    placeholder: "비밀번호를 입력해 주세요",
-                    type: "password",
-                  },
-                  {
-                    id: "confirmPassword",
-                    label: "비밀번호 확인",
-                    placeholder: "비밀번호를 다시 한 번 입력해 주세요",
-                    type: "password",
-                  },
-                ].map(({ id, label, placeholder, type }) => (
-                  <div key={id} className="flex flex-col gap-[16px]">
-                    <label className="text-[20px]">{label}</label>
-                    <input
-                      id={id}
-                      type={type}
-                      placeholder={placeholder}
-                      value={form[id as FormKey]}
-                      onChange={handleChange}
-                      className={`w-full rounded-[16px] border p-[14px] focus:border-[#F9502E] focus:outline-none ${
-                        errors[id as FormKey]
-                          ? "border-[#FF4F64]"
-                          : "border-[#E6E6E6]"
-                      }`}
-                    />
-                    {errors[id as FormKey] && (
-                      <p className="text-[16px] text-[#FF4F64]">
-                        {errors[id as FormKey]}
-                      </p>
-                    )}
-                  </div>
-                ))}
-              </div>
-
-              <button
-                type="submit"
-                disabled={!canSubmit}
-                className={`className="w-full " cursor-pointer rounded-[16px] p-[14px] font-semibold ${
-                  canSubmit
-                    ? "cursor-pointer bg-[#F9502E] text-[#FFFFFF]"
-                    : "cursor-not-allowed bg-[#D9D9D9] text-[#FFFFFF]"
-                }`}
-              >
-                시작하기
-              </button>
-            </form>
-
-            <div className="mx-auto flex w-[300px] flex-row gap-[8px] text-[20px]">
-              <p>이미 무빙 회원이신가요?</p>
-              <a className="font-semibold text-[#F9502E] underline">로그인</a>
-            </div>
+              시작하기
+            </button>
           </div>
 
-          <div className="mx-auto flex flex-col gap-[32px] text-center text-[20px]">
-            <p>SNS 계정으로 간편 가입 하기</p>
-            <div className="mx-auto flex flex-row gap-[8px]">
-              <button>구글</button>
-              <button>네이버</button>
-              <button>카카오</button>
+          <div className="mx-auto flex w-full max-w-[640px] flex-col justify-center gap-[48px] text-[#474643]">
+            <div className="flex w-full max-w-[640px] flex-col justify-center gap-[8px] text-center">
+              <div className="mx-auto h-[100px]">
+                <img
+                  src="/assets/logo.svg"
+                  alt="무빙 로고"
+                  width={200}
+                  height={80}
+                ></img>
+              </div>
+              <div className="mx-auto flex flex-row gap-[8px] text-[20px]">
+                <p>기사님이신가요?</p>{" "}
+                <a className="font-semibold text-[#F9502E] underline">
+                  기사님 전용 페이지
+                </a>
+              </div>
+            </div>
+            <div className="flex w-full flex-col gap-[24px]">
+              <form className="flex flex-col gap-[56px]">
+                <div className="mx-auto flex w-full flex-col gap-[32px]">
+                  <div className="flex flex-col gap-[16px]">
+                    <label className="text-[20px]">이름</label>
+                    <input
+                      id="username"
+                      type="text"
+                      placeholder="성함을 입력해 주세요"
+                      className="w-full rounded-[16px] border border-[#E6E6E6] p-[14px] text-[16px] focus:border-[#F9502E] focus:outline-none"
+                    />
+                  </div>
+
+                  <div className="flex flex-col gap-[16px]">
+                    <label className="text-[20px]">이메일</label>
+                    <input
+                      id="email"
+                      type="email"
+                      placeholder="이메일을 입력해 주세요"
+                      className="w-full rounded-[16px] border border-[#E6E6E6] p-[14px] focus:border-[#F9502E] focus:outline-none"
+                    />
+                  </div>
+
+                  <div className="flex flex-col gap-[16px]">
+                    <label className="text-[20px]">전화번호</label>
+                    <input
+                      id="phone"
+                      type="tel"
+                      placeholder=" 전화번호를 입력해 주세요 "
+                      className="w-full rounded-[16px] border border-[#E6E6E6] p-[14px] focus:border-[#F9502E] focus:outline-none"
+                    />
+                  </div>
+
+                  <div className="flex flex-col gap-[16px]">
+                    <label className="text-[20px]">비밀번호</label>
+                    <input
+                      id="password"
+                      type="password"
+                      placeholder=" 비밀번호를 입력해 주세요 "
+                      className="w-full rounded-[16px] border border-[#E6E6E6] p-[14px] focus:border-[#F9502E] focus:outline-none"
+                    />
+                  </div>
+
+                  <div className="flex flex-col gap-[16px]">
+                    <label className="text-[20px]">비밀번호 확인</label>
+                    <input
+                      id="confirmPassword"
+                      type="password"
+                      placeholder=" 비밀번호를 다시 한 번 입력해 주세요 "
+                      className="w-full rounded-[16px] border border-[#E6E6E6] p-[14px] focus:border-[#F9502E] focus:outline-none"
+                    />
+                  </div>
+                </div>
+
+                <button
+                  type="submit"
+                  className="w-full cursor-pointer rounded-[5px] bg-[#D9D9D9] p-[14px] font-semibold text-[#FFFFFF]"
+                >
+                  시작하기
+                </button>
+              </form>
+
+              <div className="mx-auto flex w-[300px] flex-row gap-[8px] text-[20px]">
+                <p>이미 무빙 회원이신가요?</p>
+                <a className="font-semibold text-[#F9502E] underline">
+                  로그인
+                </a>{" "}
+              </div>
+            </div>
+
+            <div className="mx-auto flex flex-col gap-[32px] text-center text-[20px]">
+              <p>SNS 계정으로 간편 가입 하기</p>
+              <div className="mx-auto flex flex-row gap-[8px]">
+                <div>구글</div>
+                <div>네이버</div>
+                <div>카카오</div>
+              </div>
             </div>
           </div>
         </div>
