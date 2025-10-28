851c7d2a56e0e17b57d0090e9d81d3cc695ea985
yewonlee1211 <yewonlee1211@korea.ac.kr>
2025-10-28 18:56:26 +0900

    리다이렉트 미들웨어 작성 #44 #55 #111

---
+++ app/auth/success/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useAuthStore } from "@/contexts/authStore";
+import { onLoginSuccess } from "@/hooks/useLogin";
+import { useQueryClient } from "@tanstack/react-query";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+export default function OAuthSuccessPage() {
+  const searchParams = useSearchParams();
+  const type = searchParams.get("type");
+  const queryClient = useQueryClient();
+  const { setAuth } = useAuthStore();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (type === "customer") {
+      onLoginSuccess("customer", queryClient, setAuth, router);
+    } else if (type === "mover") {
+      onLoginSuccess("mover", queryClient, setAuth, router);
+    }
+  }, [type]);
+
+  return <div>간편 로그인 성공! 잠시만 기다려 주세요.</div>;
+}
